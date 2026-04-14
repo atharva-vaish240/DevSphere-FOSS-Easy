1ad895ac49198e68932f2f5d217cb435b328a76b
atharva-vaish240 <atharva.vaish240@gmail.com>
2026-04-14 18:12:09 +0530

    Fixed the solution

--- solution1.cpp
+++ solution1.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
     int t; cin >> t;
     while(t--){
         long long n, k;
         cin >> n >> k;
-        long long count = 0, current = 0;
-        while (count < k) {
-            current++;
-            if (current % n != 0) count++;
-        }
-        cout << current << endl;
+        cout << (k-1)/(n-1) + k << endl;
     }
     return 0;
 }
